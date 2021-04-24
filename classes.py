71089ac40c914e58c42da3b9d85a39972476efa2
Sravya <65549677+Sravya2007@users.noreply.github.com>
2021-04-24 20:13:29 +0530

    pip

--- classes.py
+++ classes.py
@@ -1,17 +1,24 @@
 class Student(object):
-    def __init__(self, name, age, gender, level, grades = None):
+    def __init__(self, name, age, gender, level, grades=None):
         self.name = name
         self.age = age
         self.gender = gender
         self.level = level
-        self.grades = grades
+        self.grades = grades or {}
+
     def setGrade(self, course, grade):
         self.grades[course] = grade
+
     def getGrade(self, course):
         return self.grades[course]
+
     def getGPA(self):
         return sum(self.grades.values())/len(self.grades)
-john = Student("John", 12, "male", 6, {"Math": 3.3})
-sravya = Student("Sravya", 14, "female", 9, {"Science": 9})
+
+# Define some students
+john = Student("John", 12, "male", 6, {"math":3.3})
+jane = Student("Jane", 12, "female", 6, {"math":3.5})
+
+# Now we can get to the grades easily
 print(john.getGPA())
-print(sravya.getGPA())
+print(jane.getGPA())
